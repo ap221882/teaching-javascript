d77896d4dd6f4c6b43401fbb867db33238886285
ajay <ap221882@gmail.com>
2023-03-15 22:46:47 +0530

    feat: operations

---
+++ 01-Primer/02-operations.js
@@ -0,0 +1,15 @@
+//> Operand-Operator-Operand OR Operator-Operand
+//~* unary & binary
+3 + 4;
+43 - 1;
+"Ajay" + "Pathak";
+!false;
+//^ Equality
+3 == "3";
+3 === "3";
+4.0 === 4;
+5 < 0;
+//^ Logical Operators
+3 && 4;
+3 || 4;
+true || false;
