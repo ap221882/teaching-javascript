c74c056031a7f816b619fae07c0dcd5a07a492d9
ajay <ap221882@gmail.com>
2023-03-15 22:41:45 +0530

    feat: values in JS

---
+++ 01-Primer/values.js
@@ -0,0 +1,19 @@
+//> Forget Everything
+
+// Number
+1;
+2.23;
+// String
+("Hello");
+// Boolean
+true;
+false;
+// Emptiness
+null;
+undefined;
+// More complex Data Structures - NON PRIMITIVE
+// A for Arrays, not A for apple - 0 indexed - can contain anything
+[1, 2, 3];
+{
+  name: "Ajay"; //> HashMaps
+}
