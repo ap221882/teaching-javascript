2993cb8d8a88c2b66d563e0acc8bb23e77c7a97a
ajay <ap221882@gmail.com>
2023-03-16 10:16:48 +0530

    feat: functions in JS

---
+++ 01-Primer/07-functions.js
@@ -0,0 +1,8 @@
+//> called as procedures - DRY
+//% They return something - STACKFRAME ARCHITECTURE
+
+function printName(value) {
+  console.log(`The name is ${value}`);
+}
+
+//> Three types - expressions, arrow/lambda, declarations
