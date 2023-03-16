342e6a29152edba2288a7735ebe32b5294ccb454
ajay <ap221882@gmail.com>
2023-03-16 20:36:21 +0530

    feat: 2.1 primitive types

---
+++ 02-typesNcoercion/01-primitiveTypes.js
@@ -0,0 +1,12 @@
+//> Primitive types
+//% Everything in JavaScript is not an object, i.e., primitive types
+
+//* string, boolean, undefined, null, Symbol, number, BigInt
+//# variables dont have type in JS like CPP and Java but values do have types - Bcoz JS is a dynamic language
+//^ undefined can be implicit and explicit
+typeof null; // BUG
+function a() {}
+typeof a; // strange behaviour
+typeof someArray;
+//> Converting types
+//> Checking equality
