df82d9621d9401ad446618598204d255731a9eec
ajay <ap221882@gmail.com>
2023-03-17 10:20:40 +0530

    feat: coercion

---
+++ 02-typesNcoercion/04-type-coercion.js
@@ -0,0 +1,9 @@
+//> Coercion is implicit - in a dynamically and weakly typed language
+/**
+ * * + involves coercion to a string type when used as a binary operator and
+ * * + involves coercion to a number type when used as a unary operator
+ * */
+var marks = 100;
+console.log(`I got ${marks + ''} in the Mathematics`);
+//% Number and String to the rescue
+//> Conversion is explicit - When we need it, like integrations with another languages
