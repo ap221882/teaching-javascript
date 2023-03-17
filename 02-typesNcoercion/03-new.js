613dec029b82b031b917caae4193936deedfddd2
ajay <ap221882@gmail.com>
2023-03-17 10:12:15 +0530

    feat: new keyword used for creating object

---
+++ 02-typesNcoercion/03-new.js
@@ -0,0 +1,12 @@
+//> copied from Java
+
+//# use new
+Object, Array, Function, Date, RegExp, Error;
+//# don't use new
+//> Involved in type conversion
+String, Number, Boolean;
+
+const yesterday = new Date('March 16, 2023');
+console.log(yesterday.toUTCString());
+
+var someString = String('Ajay');
