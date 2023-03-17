4f85d7b7cae67e39178099bff72aa3ef16393276
ajay <ap221882@gmail.com>
2023-03-17 10:11:54 +0530

    feat: Not a Number

---
+++ 02-typesNcoercion/02-NaN.js
@@ -0,0 +1,9 @@
+//> Not A Number (NaN) - indicate invalid numeric operation
+
+//% It is a number
+
+// When Js is confused with operation with numbers
+'string' / 2;
+//# to check it
+NaN !== NaN;
+Number.isNaN(NaN);
